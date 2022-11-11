1e33957e5cd51e85298704d35c37b4aedb8906cb
Devin Sharpe <devinrsharpe@gmail.com>
2022-11-11 01:25:01 -0500

    chore: add prisma plugin

---
+++ server/plugins/prisma.ts
@@ -0,0 +1,21 @@
+import { FastifyPluginAsync } from "fastify";
+import { PrismaClient } from "@prisma/client";
+import fp from "fastify-plugin";
+
+declare module "fastify" {
+  interface FastifyInstance {
+    prisma: PrismaClient;
+  }
+}
+
+const prismaPlugin: FastifyPluginAsync = fp(async (server) => {
+  const prisma = new PrismaClient();
+  await prisma.$connect();
+  server.decorate("prisma", prisma);
+  server.addHook(
+    "onClose",
+    async (server) => await server.prisma.$disconnect()
+  );
+});
+
+export default prismaPlugin;
