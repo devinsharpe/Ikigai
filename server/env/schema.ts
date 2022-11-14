b6450e44e2b98078e1d1d86010d62284ba22dde6
Devin Sharpe <devinrsharpe@gmail.com>
2022-11-13 20:00:39 -0500

    chore: update auth env vars

--- server/env/schema.ts
+++ server/env/schema.ts
@@ -3,6 +3,11 @@ import { z } from "zod";
 export const schema = z.object({
   DATABASE_URL: z.string().url(),
   DEV_SUBDOMAIN: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  HOST_URL: z.string(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  PORT: z.string()
+  PORT: z.string(),
+  SESSION_SALT: z.string(),
+  SESSION_SECRET: z.string()
 });
